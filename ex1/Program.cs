584b3570cc7595768e4636fcb2a91b1d71946240
Giorg <Giorg@PCJEAN>
2024-11-12 14:32:15 +0100

    Ajoutez des fichiers projet.

---
+++ ex1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclarations
+            float moyenne;
+
+            Console.Write("saisisez la moyenne : ");
+            moyenne = float.Parse(Console.ReadLine());
+
+            // appréciations
+            if(moyenne <10)
+            {
+                
+                Console.WriteLine("vous étes recaler ");
+
+            }
+            else
+            {
+                if (moyenne < 12)
+                {
+                    Console.WriteLine(" passable");
+                }
+
+                else
+                {
+                    if (moyenne < 14) 
+                    {
+                        Console.WriteLine("assez bien");
+                    }
+
+                    else
+                    {
+                        if (moyenne < 16) 
+                        {
+                            Console.WriteLine("bien");
+                        }
+                        else
+                            {
+                            Console.WriteLine(" très bien");
+                            }
+                    }
+                }
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
